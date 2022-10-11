17958802de06bac1e51ea04c54c71bfa27907ccd
Harrytx426 <30800174+Harrytx426@users.noreply.github.com>
2022-10-11 20:24:33 +0200

    Add files via upload

--- assets/cmbip.sh
+++ assets/cmbip.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 echo "Has seleccionado cambiar la IP"
 read -p "Deseas configurar Estatica o DHCP (E/D)" ed
-if [ $ed = 'd' ]||[ $ed = 'D' ]; then
-    cp ./config/dhcp.conf /etc/netplan/01-installer-config.yaml
+if [ $ed = 'd' ] || [ $ed = 'D' ]; then
+    cp ./config/dhcp.conf /etc/netplan/01-installer-config.yaml /etc/dhcp/ 2>/dev/null
+    if [ $? = 0 ]; then
+        echo "Configurado con exito"
+        else
+       echo "Algo no salio como se esperaba"
+       
+    fi
     sudo netplan apply
-elif [ $ed = 'e' ]||[ $ed = 'E' ]; then
-    cp ./config/static.conf /etc/netplan/01-network-manager-all.yaml
     sudo nano /etc/netplan/01-network-manager-all.yaml
+elif [ $ed = 'e' ] || [ $ed = 'E' ]; then
+    cp ./config/static.conf /etc/netplan/01-network-manager-all.yaml 2>/dev/null
+    if [ $? = 0 ]; then
+        echo "Configurado con exito"
+        else
+       echo "Algo no salio como se esperaba"
+       
+    fi
     sudo netplan apply
+    sudo nano /etc/netplan/01-network-manager-all.yaml
 else
     echo "Sintaxis equivocada"
 fi
